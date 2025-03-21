ed5a2019e485675c84dbf38458741d82318fb6e3
damilyaamangeldykyzy <damilyaamangeldykyzy@gmail.com>
2025-03-21 15:25:37 +0500

    search handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Trends from "./components/Trends/Trends";
 import Footer from "./components/Footer/Footer";
+import TrendInfo from "./components/TrendInfo/TrendInfo";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/trends" element={<Trends />} />
+        <Route path="/trend/:trendName" element={<TrendInfo />} /> 
       </Routes>
       <Footer />
     </Router>
